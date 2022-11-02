c479c054ce2f5a074c7a7ac7579b3e26b3fdfae2
ShazilHussain <116807199+ShazilHussain@users.noreply.github.com>
2022-11-03 03:43:44 +0500

    javascript assignment 2 practice
    javascript assignment 2 practice

---
+++ app.js
@@ -0,0 +1,58 @@
+//chapter: 1 start.
+var value = prompt("Enter a Value");
+console.log(value)
+// chapter: 1 complete.
+
+// chapter: 2 start.
+var x = prompt("Where does the Pope live?");
+ if (x === "correct") {
+ console.log("Correct!");
+}
+// chapter: 2 complete.
+
+// chapter: 3 start.
+var b = prompt("shazil");
+if(b === "shazil"){
+    console.log("correct")
+
+}
+// chapter: 3 complete.
+
+ // chapter: 4 start.
+var officer = prompt("officer");
+if(officer === "true"){
+    console.log("work is submited")
+}else if( officer==="false"){
+    console.log("work is not submited")
+}else{
+    console.log("wapsi ghar" )
+}
+ // chapter: 4 complete.
+
+ // chapter: 5 start.
+ var weight = prompt("Enter a Weigth")
+ var time = prompt("Enter a time")
+ var age = prompt("Enter a age")
+ var gender = prompt("Enter a gendermale")
+
+ if(weight > 300 || time < 6 || age > 17 || gender === "male"){
+    console.log("Come to our tryout!")
+ }else{
+     console.log("Come to our cookout!")
+ }
+ // chapter: 5 complete.
+
+ // chapter: 6 start.
+ var male = prompt("Enter a gender")
+ var male1 = prompt("Enter a female")
+if(male === "gender"){
+    console.log("gender Name")
+    if(male1 === "female"){
+        console.log("enter a correct gender")
+    }else{
+        console.log("uncorrect gender")
+    }
+}else{
+    console.log("not a male name")
+}
+ // chapter: 6 complete.
